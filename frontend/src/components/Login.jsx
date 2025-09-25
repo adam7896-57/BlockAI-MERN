be67004899423f91590b8e467b77f6b63ae00f1b
foxiesx <foxiesx@gmail.com>
2025-09-25 05:26:03 +0200

    Refactor usability tests
    Co-authored-by: victorykop polar.star.ssp@gmail.com
    Co-authored-by: foxiesx foxiesx@gmail.com

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="mb-6 p-2 w-full border rounded"
+          className="mb-6 p-2 w-full border rounded-full"
         />
         <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded w-full">
           Login
